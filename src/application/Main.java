670b04a745569a80293a277e7955484cd1fd0e26
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2022-05-19 14:55:03 -0300

    method call stack

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,8 +6,25 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+   
+		method1();
 		
+		System.out.println("End of program");
+	}
+	
+	public static void method1(){
+		
+		System.out.println("**** METHOD1 START ***");
+		method2();
+		System.out.println("**** METHOD1 STOP ***");
+		
+	}
+	
+	public static void method2() {
+		
+		System.out.println("**** METHOD2 START ***");
 		Scanner sc = new Scanner(System.in);
+		
 		try {
 			
 			String[] vect = sc.nextLine().split(" ");
@@ -19,19 +36,22 @@ public class Main {
 		catch(ArrayIndexOutOfBoundsException erro001 ) {
 			
 			System.out.println("Invalid position!");
+			erro001.printStackTrace();
+			
 			
 		}
 		
 		catch(InputMismatchException erro002){
 			
 			System.out.println("Input error");
+			erro002.printStackTrace();
 			
 		}
 		
 		
-		System.out.println("End of program");
-		
+		System.out.println("**** METHOD2 STOP ***");
 		sc.close();
+		
 	}
 
 }
