1f1862627c9c0b4cbfeb411afcb92c464ff50a07
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2022-05-19 18:08:13 -0300

    Very bad solution

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,46 +1,53 @@
 package application;
 
-
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+
+import Model.entities.Reservation;
 
 public class Main {
 
-	public static void main(String[] args) {
-    
-		File file = new File("C:\\temp\\in.txt");
-		Scanner sc = null;
+	public static void main(String[] args) throws ParseException{
+ 
 		
-		try {
-			
-			sc = new Scanner(file);
-			
-			while (sc.hasNextLine()){
-				
-				System.out.println(sc.nextLine());
-			 }
-			}
-			
-		catch(FileNotFoundException erro ) {
-			
-			System.out.println("Error opning the file: " + erro.getMessage());
-		}
-		finally {
-			
-			if(sc != null) {
-				
-				sc.close();
-				
-				System.out.println("Scanner close");
-			}
-			
-			System.out.println("Finally block executed");
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		  }
-		}
+		System.out.print("Room number: ");
+		int roomNumber = sc.nextInt();
+		
+	
+		
+		System.out.print("Check-in date (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+	
+		
+		
+		System.out.print("Check-out date (dd/MM/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
 
 		
+		if (!checkOut.after(checkIn)) {
+			
+			System.out.println("Error in reservation: Check-out date must be after check-in date !");
+		}
+		else {
+			
+			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+		}
+		
+		
+		
+		
+		System.out.print("Resevation:");
+	
+	
 	}
+
+		
+}
 
 
