4a89c045ba1b8855b2619ce7cf2c2209ce409e8d
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2022-05-19 15:23:07 -0300

    Learning block finally

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,57 +1,46 @@
 package application;
 
-import java.util.InputMismatchException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-   
-		method1();
-		
-		System.out.println("End of program");
-	}
-	
-	public static void method1(){
-		
-		System.out.println("**** METHOD1 START ***");
-		method2();
-		System.out.println("**** METHOD1 STOP ***");
-		
-	}
-	
-	public static void method2() {
-		
-		System.out.println("**** METHOD2 START ***");
-		Scanner sc = new Scanner(System.in);
+    
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
 		
 		try {
 			
-			String[] vect = sc.nextLine().split(" ");
-			int  position = sc.nextInt();
-			System.out.println(vect[position]);
+			sc = new Scanner(file);
 			
+			while (sc.hasNextLine()){
+				
+				System.out.println(sc.nextLine());
+			 }
+			}
+			
+		catch(FileNotFoundException erro ) {
+			
+			System.out.println("Error opning the file: " + erro.getMessage());
 		}
+		finally {
+			
+			if(sc != null) {
+				
+				sc.close();
+				
+				System.out.println("Scanner close");
+			}
+			
+			System.out.println("Finally block executed");
 		
-		catch(ArrayIndexOutOfBoundsException erro001 ) {
-			
-			System.out.println("Invalid position!");
-			erro001.printStackTrace();
-			
-			
+		  }
 		}
-		
-		catch(InputMismatchException erro002){
-			
-			System.out.println("Input error");
-			erro002.printStackTrace();
-			
-		}
-		
-		
-		System.out.println("**** METHOD2 STOP ***");
-		sc.close();
+
 		
 	}
 
-}
+
